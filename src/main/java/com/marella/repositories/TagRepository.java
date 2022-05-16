ebda20c3dae970260dfa9f843ccab8901450ad42
g-o-g-y <larkin.cema@outlook.com>
2022-05-17 00:24:30 +0300

    feat(repositories/TagRepository): added method to search for a tag by name

--- src/main/java/com/marella/repositories/TagRepository.java
+++ src/main/java/com/marella/repositories/TagRepository.java
@@ -4,6 +4,10 @@ import com.marella.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+
 }
