f90f46336e0ee087f1f648bd47b555fc48329a1f
g-o-g-y <larkin.cema@outlook.com>
2022-05-19 14:41:47 +0300

    feat(repositories/PermissionRepository): method finding by User and Space added

--- src/main/java/com/marella/repositories/PermissionRepository.java
+++ src/main/java/com/marella/repositories/PermissionRepository.java
@@ -1,9 +1,14 @@
 package com.marella.repositories;
 
 import com.marella.models.Permission;
+import com.marella.models.Space;
+import com.marella.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Optional<Permission> findByUserAndSpace(User user, Space space);
 }
