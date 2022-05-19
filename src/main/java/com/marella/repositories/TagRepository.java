4523a59bb3c01c502984e42533b903c22e0a3a28
g-o-g-y <larkin.cema@outlook.com>
2022-05-19 14:42:33 +0300

    feat(repositories/TagRepository): method finding by tagName and Space added

--- src/main/java/com/marella/repositories/TagRepository.java
+++ src/main/java/com/marella/repositories/TagRepository.java
@@ -1,5 +1,6 @@
 package com.marella.repositories;
 
+import com.marella.models.Space;
 import com.marella.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Optional<Tag> findByName(String name);
+    Optional<Tag> findByNameAndSpace(String name, Space space);
 
 }
