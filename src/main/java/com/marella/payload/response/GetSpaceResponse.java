3fa9c6edbc00151163756ddfd8e0223baeedc264
g-o-g-y <larkin.cema@outlook.com>
2022-05-20 15:36:58 +0300

    feat(payload/response/GetSpaceResponse)

---
+++ src/main/java/com/marella/payload/response/GetSpaceResponse.java
@@ -0,0 +1,41 @@
+package com.marella.payload.response;
+
+import com.marella.models.Permission;
+import com.marella.models.Space;
+import com.marella.models.Tag;
+import com.marella.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetSpaceResponse {
+    Space space;
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append(String.format("{\"name\":\"%s\",", space.getName()));
+
+        String prefix = "\"members\":[";
+        for(Permission permission: space.getPermissions()){
+            response.append(prefix);
+            response.append(permission.getUser().getId());
+            prefix = ",";
+        }
+        response.append("],");
+
+        prefix = "\"tags\":[";
+        for(Tag tag: space.getTags()){
+            response.append(prefix);
+            response.append(String.format("\"%s\"", tag.getName()));
+            prefix = ",";
+        }
+        response.append("],");
+
+        response.append(String.format("\"is_public\":%s}", space.isPublic()));
+        return response.toString();
+    }
+}
