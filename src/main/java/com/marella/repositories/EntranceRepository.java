6b8192d187f95fb25ecd4d38f7db977e425fffc9
g-o-g-y <larkin.cema@outlook.com>
2022-05-24 01:07:15 +0300

    feat(repositories/EntranceRepository): added findByUserAndSpace method

--- src/main/java/com/marella/repositories/EntranceRepository.java
+++ src/main/java/com/marella/repositories/EntranceRepository.java
@@ -1,9 +1,14 @@
 package com.marella.repositories;
 
 import com.marella.models.Entrance;
+import com.marella.models.Space;
+import com.marella.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EntranceRepository extends JpaRepository<Entrance, Long> {
+    Optional<Entrance> findByUserAndSpace(User user, Space space);
 }
