2e23ba5174b40e65520768464f8efced4ef551d2
g-o-g-y <larkin.cema@outlook.com>
2022-05-28 13:36:38 +0300

    feat(models): Report model

---
+++ src/main/java/com/marella/models/Report.java
@@ -0,0 +1,71 @@
+package com.marella.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "Report")
+@Table(name = "report")
+public class Report {
+    @Id
+    @SequenceGenerator(
+            name = "report_sequence",
+            sequenceName = "report_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "report_sequence"
+    )
+    Long id;
+
+    @OneToOne
+    @JoinColumn(
+            name = "declarer_user_id",
+            referencedColumnName = "id",
+            nullable = false,
+            foreignKey = @ForeignKey(
+                    name = "declarer_user_id_fk"
+            )
+    )
+    User declarerUser;
+
+    @OneToOne
+    @JoinColumn(
+            name = "accused_user_id",
+            referencedColumnName = "id",
+            nullable = false,
+            foreignKey = @ForeignKey(
+                    name = "accused_user_id_fk"
+            )
+    )
+    User accusedUser;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    String message;
+
+    public Report(User declarerUser, User accusedUser, String message) {
+        this.declarerUser = declarerUser;
+        this.accusedUser = accusedUser;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"report_id\":" + id +
+                ",\"message\"" + message + '\"' +
+                ",\"declarerUser\":" +
+                String.format("{%d,\"%s\",\"%s\"}", declarerUser.getId(), declarerUser.getEmail(), declarerUser.getUsername()) +
+                ",\"accusedUser\":"  +
+                String.format("{%d,\"%s\",\"%s\"}", accusedUser.getId(), accusedUser.getEmail(), accusedUser.getUsername()) +
+                "}";
+    }
+}
